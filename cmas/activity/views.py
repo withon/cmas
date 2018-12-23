4e4938ca968b056063b36043cbc57232b62f6285
withon <469698349@qq.com>
2018-12-23 20:58:31 +0800

    index视图完成

--- cmas/activity/views.py
+++ cmas/activity/views.py
@@ -3,10 +3,15 @@ from django.http import HttpResponse
 from .models import User, Activity, Registration, Mnotice, Sysnotice
 
 # Create your views here.
+
+
 def index(request):
     content = {}
     all_activities = Activity.objects.all()
     latest_activity = all_activities.order_by('-rtime')[0]
     content['latest_activity'] = latest_activity
+    all_sysnotices = Sysnotice.objects.all()
+    latest_sysnotice = all_sysnotices.order_by('time')[0]
+    content['latest_sysnotice'] = latest_sysnotice
 
     return render(request, 'index.html', content)
