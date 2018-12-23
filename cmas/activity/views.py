6a8661045f4ac540db4b2c0105e43ec79a19a854
withon <469698349@qq.com>
2018-12-23 12:22:01 +0800

    开始写视图，pylint报错未解决

--- cmas/activity/views.py
+++ cmas/activity/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
+from .models import User, Activity, Registration, Mnotice, Sysnotice
 
 # Create your views here.
 def index(request):
     content = {}
-    
+    all_activities = Activity.objects.all()
+    latest_activity = all_activities.order_by('-rtime')[0]
+    content['latest_activity'] = latest_activity
 
     return render(request, 'index.html', content)
